021ba9e03ff22fe7bd7809159934a9df22d73151
Anna Sothichenko <ASotnichenko@alfabank.ru>
2021-12-18 21:21:58 +0300

    задание со количеством повторяющихся элементов

--- src/main/java/RepeatNumber.java
+++ src/main/java/RepeatNumber.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class RepeatNumber {
@@ -6,19 +7,20 @@ public class RepeatNumber {
         {
             Scanner console = new Scanner(System.in);
             String my_string = console.nextLine();
-            String[] my_array = {my_string};
 
-            for (int i = 0; i < my_array.length-1; i++)
-            {
-                for (int j = i+1; j < my_array.length; j++)
-                {
-                    if( (my_array[i].equals(my_array[j])) && (i != j) )
-                    {
-                        System.out.println("Пвторяющиеся элементы : "+my_array[j]);
-                    }
-                }
-            }
+            List<String> list = new ArrayList<String>();
+            list.add(my_string);
+
+            Map<String, Long> result =
+                    list.stream().collect(
+                            Collectors.groupingBy(
+                                    Function.identity(), Collectors.counting()
+                            )
+                    );
+
+            System.out.println(result);
+        }
         }
 
     }
-}
+
--- src/main/java/Weather.java
+++ src/main/java/Weather.java
@@ -4,13 +4,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Calendar;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Weather {
-    @Scheduled(cron = "0 0 * * * *")
-    public static void main(String[] args) throws IOException {
+    interface Runnable
+    {
+        void run();
+    }
+
+    public Runnable run()  throws IOException  {
         FileOutputStream weatherToday = new FileOutputStream("weatherToday.txt");
 
         String url = "https://api.openweathermap.org/data/2.5/onecall?lat=59&lon=30&exclude=1586468027&appid=57e839140068ddde69d9c9550fac3718";
@@ -32,6 +38,13 @@ public class Weather {
         System.out.println(response.toString());
         weatherToday.write(Integer.parseInt(response.toString()));
 
+    ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+    Long midnight= LocalDateTime.now().until(LocalDate.now().plusDays(1).atStartOfDay(), ChronoUnit.MINUTES);
+    Runnable run = run();
+    scheduler.scheduleAtFixedRate((java.lang.Runnable) run, midnight, TimeUnit.DAYS.toMinutes(1), TimeUnit.MINUTES);
+        return run;
     }
+
 }
 
